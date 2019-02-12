2d79043c362e12b66e337101c5e82e7a32e0c4b5
sugheerth <42635206+sugheerth@users.noreply.github.com>
2019-02-12 12:12:10 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,5 +3,7 @@ public static void main(){
   ckeck chk = new check();
   chk.show();
   int num = 0 ;
+  Map<String,String> map = new HashMap<String,String>();
+  Iterator<?> it = map.keyset.iterator();
   
 }
